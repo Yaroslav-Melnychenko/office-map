d2ad8d5c4fcaf9ea77796875f3741119e819ceb8
Yaroslav Melmychenko <yaroslav.melnychenko@chisw.com>
2019-05-27 16:59:00 +0300

    click on place

--- src/components/FloorSvg5/FloorSvg5.jsx
+++ src/components/FloorSvg5/FloorSvg5.jsx
@@ -1,29 +1,30 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import FloorPlan from './FloorPlan';
 import './FloorSvg5.scss';
 
 class FloorSvg5 extends Component {
 
-  state ={
-    employees: null
+  state = {
+    selected: null
   }
 
-  componentDidMount() {
-    axios.get('http://localhost:4000/floor5').then(responce => {
-      this.setState({ employees: responce.data });
-    })
+  placeClick = (id) => {
+    const { selectPlace } = this.props;
+    this.setState({ selected: id });
+    selectPlace(id);
   }
 
   render() {
-    const { employees } = this.state;
+
+    const { employees } = this.props;
+    const { selected } = this.state;
+
     return (
-      <svg width="376.16mm" height="251.84mm" viewBox="0 0 1066.28 713.89">
+      <svg viewBox="300 -300 600 600">
         {
           employees && employees.map(employee => (
-            <g key={employee.id} className="one-place">
-              <path className="st0" d={employee.d} />
-              {/* <path className="st1" d={employee.d2} /> */}
+            <g key={employee.id} className="one-place" onClick={() => this.placeClick(employee.id)} >
+              <path className={`st0 ${ employee.id === selected ? 'selected' : null }`} d={employee.d} />
             </g>
           ))
         }
--- src/components/FloorTab/FloorTab.jsx
+++ src/components/FloorTab/FloorTab.jsx
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import FloorSvg5 from '../FloorSvg5';
 import './FloorTab.scss';
 
 class FlooTab extends Component {
+
+  state ={
+    employees: [],
+    place: null
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:4000/floor5').then(responce => {
+      this.setState({ employees: responce.data });
+    })
+  }
+
+  selectPlace = (place) => {
+    this.setState({ place });
+  }
+
   render() {
+
+    const { employees, place } = this.state;
     return (
+
       <div className="floor-container">
         <div className="map">
-          <FloorSvg5 />
+        <FloorSvg5 employees={employees} selectPlace={this.selectPlace} />
         </div>
-        <div className="worker">worker</div>
+        <div className="worker">
+          <p>Place: <span>{ place }</span></p>
+        </div>
       </div>
     );
   }
